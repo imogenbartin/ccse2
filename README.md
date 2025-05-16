f214e7a45802642214bd7a011b17f8a786ef7334
imogenbartin <157552603+imogenbartin@users.noreply.github.com>
2025-05-16 11:31:31 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# CCSE1
+# pip install -r requirements.txt
+# run main.py in a python IDE to run the website
+
---
+++ main.py
@@ -0,0 +1,8 @@
+from Website import createApp #importing the function from __init__.py
+
+
+app = createApp()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
